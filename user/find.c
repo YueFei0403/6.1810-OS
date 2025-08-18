7bd7e5e20d1fc2108b84a11dcf08c0755fcbaab5
Yue Fei <yuefei9943@gmail.com>
2025-08-18 15:52:50 -0400

    Added regex matcher for find.c

--- user/find.c
+++ user/find.c
@@ -4,6 +4,93 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+/********************** Regexp matcher from Kernighan & Pike,******************************/
+// The Practice of Programming, Chapter 9, or
+// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+// ^ -> match start of string
+// $ -> match end of string
+// . -> match any one character
+// * -> match zero or more of the preceding character
+
+/* match: search for regexp anywhere in text */
+int match(char *re, char *text)
+{
+    if (re[0] == '^')
+        return matchhere(re+1, text);
+    do { // must look even if string is empty
+        if (matchhere(re, text))
+            return 1;
+    } while(*text++ != '\0');
+    return 0;
+}
+
+/* matchhere: search for regexp at beginning of text */
+int matchhere(char *re, char *text)
+{
+    if (re[0] == '\0')
+        return 1;
+    if (re[1] == '*')
+        return matchstar(re[0], re+2, text);
+    if (re[0] == '$' && re[1] == '\0')
+        return *text == '\0';
+    if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+        return matchhere(re+1, text+1);
+    return 0;
+}
+
+/* matchstar: search for c*regexp at beginning of text */
+int matchstar(int c, char *regexp, char *text)
+{
+    do {    /* a * matches zero or more instances */
+        if (matchhere(regexp, text))
+            return 1;
+    // text pointer must advance, hence the order of comparison matters
+    } while (*text != '\0' && (*text++ == c || c == '.')); 
+    return 0;
+}
+
+/*****************************************************************
+ * Glob-Style Matcher
+ * '*' -> matches zero or more chars
+ * '?' -> matches any single char
+ * no anchors (^, $) needed -> behaves like strcmp with wildcards
+ *****************************************************************/
+int globmatch(const char *pattern, const char *text) {
+    while (*pattern) {
+        if (*pattern == '*') {
+            // Collapse multiple '*' in a row
+            while (*(pattern + 1) == '*')
+                pattern++;
+
+            // Try to match rest of pattern at every tail of text
+            pattern++;
+            if (*pattern == '\0')
+                return 1; // trailing '*' matches everything
+            while (*text) {
+                if (globmatch(pattern, text))
+                    return 1;
+                text++;
+            }
+            return 0;
+        } else if (*pattern == '?') {
+            if (*text == '\0') // we expect one char matching, but the text buf ends
+                return 0;
+            pattern++;
+            text++;
+        } else {
+            if (*pattern != *text) // oops -> no match
+                return 0;
+            pattern++;
+            text++;
+        }
+    }
+    return *text == '\0';
+}
+
 
 /******************************************************************************
  * Key Idea: 
@@ -60,7 +147,7 @@ find(char *path, char *pattern)
         while (p >= path && *p != '/')
             p--;
         p++;
-        if (strcmp(p, pattern) == 0)
+        if (match(p, pattern))
             printf("%s\n", path);
         break;
 
@@ -85,6 +172,10 @@ find(char *path, char *pattern)
                 continue;
             }
 
+            //// if the name matches, print full path
+            //if (match(pattern, de.name)) {
+            //    printf("%s\n", buf);
+            //}
             // Recursively search subdirectories and files
             find(buf, pattern);
         }
@@ -100,7 +191,18 @@ main(int argc, char *argv[])
         fprintf(2, "usage: find <path> <name>\n");
         exit(1);
     }
-    find(argv[1], argv[2]);
+    // Auto expand user's pattern: 'foo' -> '^.*foo.*$'
+    char wrapped[256];
+    char *p = wrapped;
+    
+    strcpy(wrapped, "^.*");
+    p += strlen("^.*");
+
+    strcpy(p, argv[2]);
+    p += strlen(argv[2]);
+
+    strcpy(p, ".*$");
+    find(argv[1], wrapped); // wrapped ==> ^.*<pattern>.*$
     exit(0);
 }
 
